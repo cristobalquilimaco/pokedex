f27ef08f0c546e75a6b593dadaee579380b18c2c
cristobalquilimaco <quilimacox1@gmail.com>
2023-08-30 20:50:59 -0500

    Se importa el archivo de estilos loadingPage.css a loadingPage.jsx

--- src/components/LoadinPage.jsx
+++ src/components/LoadinPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import charmanderLoading from"/public/charmander-poke.gif"
+import "../components/Pokedex/styles/loadingPage.css"
 
 const LoadinPage = () => {
   return (
